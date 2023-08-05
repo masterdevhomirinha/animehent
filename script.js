2f9deac434b38b504b3139157cdc0d1e47c7aa9b
masterdevhomirinha <138265944+masterdevhomirinha@users.noreply.github.com>
2023-08-05 18:42:45 -0300

    topsanime

---
+++ script.js
@@ -0,0 +1,45 @@
+const generateButton = document.getElementById("generateButton");
+const animeImage = document.getElementById("animeImage");
+const downloadLink = document.getElementById("downloadLink");
+const randomTab = document.getElementById("randomTab");
+const girlTab = document.getElementById("girlTab");
+const boyTab = document.getElementById("boyTab");
+
+randomTab.addEventListener("click", () => {
+    randomTab.classList.add("active");
+    girlTab.classList.remove("active");
+    boyTab.classList.remove("active");
+});
+
+girlTab.addEventListener("click", () => {
+    girlTab.classList.add("active");
+    randomTab.classList.remove("active");
+    boyTab.classList.remove("active");
+});
+
+boyTab.addEventListener("click", () => {
+    boyTab.classList.add("active");
+    randomTab.classList.remove("active");
+    girlTab.classList.remove("active");
+});
+
+generateButton.addEventListener("click", () => {
+    let tab = "random";
+    
+    if (girlTab.classList.contains("active")) {
+        tab = "girl";
+    } else if (boyTab.classList.contains("active")) {
+        tab = "boy";
+    }
+
+    fetch(`https://api.waifu.pics/sfw/${tab}`)
+        .then(response => response.json())
+        .then(data => {
+            const imageUrl = data.url;
+            animeImage.src = imageUrl;
+            animeImage.style.display = "block";
+            downloadLink.href = imageUrl;
+            downloadLink.style.display = "block";
+        })
+        .catch(error => console.error("Erro ao carregar a imagem:", error));
+});
