a88c8f32e9780696aa80c5c98d4ffb5c084f5cc2
masterdevhomirinha <138265944+masterdevhomirinha@users.noreply.github.com>
2023-08-05 21:56:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,24 @@ const messageBox = document.getElementById("message-box");
 const messageInput = document.getElementById("message-input");
 const sendButton = document.getElementById("send-button");
 
-sendButton.addEventListener("click", function () {
+const chatGPT = new ChatGPT({
+    apiKey: "YOUR_OPENAI_API_KEY"
+});
+
+sendButton.addEventListener("click", async function () {
     const message = messageInput.value;
     if (message) {
-        const messageElement = document.createElement("div");
-        messageElement.className = "message";
-        messageElement.textContent = message;
-        messageBox.appendChild(messageElement);
+        const userMessageElement = document.createElement("div");
+        userMessageElement.className = "message user";
+        userMessageElement.textContent = "Você: " + message;
+        messageBox.appendChild(userMessageElement);
+
+        const response = await chatGPT.sendMessage(message);
+        const aiMessageElement = document.createElement("div");
+        aiMessageElement.className = "message ai";
+        aiMessageElement.textContent = "ChatGPT: " + response.choices[0].message.content;
+        messageBox.appendChild(aiMessageElement);
+
         messageInput.value = "";
     }
 });
