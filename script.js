cebdeeab5d81aae4b22c05c47f4c197a85f933bc
masterdevhomirinha <138265944+masterdevhomirinha@users.noreply.github.com>
2023-08-05 22:52:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
-const messageBox = document.getElementById("message-box");
-const messageInput = document.getElementById("message-input");
-const sendButton = document.getElementById("send-button");
+const content = document.getElementById("content");
+const loginForm = document.getElementById("login-form");
+const registrationForm = document.getElementById("registration-form");
+const profile = document.getElementById("profile");
+const profileImage = document.getElementById("profile-image");
 
-const chatGPT = new ChatGPT({
-    apiKey: "YOUR_OPENAI_API_KEY"
-});
+let loggedInUser = null;
 
-sendButton.addEventListener("click", async function () {
-    const message = messageInput.value;
-    if (message) {
-        const userMessageElement = document.createElement("div");
-        userMessageElement.className = "message user";
-        userMessageElement.textContent = "Você: " + message;
-        messageBox.appendChild(userMessageElement);
-
-        const response = await chatGPT.sendMessage(message);
-        const aiMessageElement = document.createElement("div");
-        aiMessageElement.className = "message ai";
-        aiMessageElement.textContent = "ChatGPT: " + response.choices[0].message.content;
-        messageBox.appendChild(aiMessageElement);
-
-        messageInput.value = "";
+document.getElementById("login-button").addEventListener("click", function () {
+    const username = document.getElementById("login-username").value;
+    const password = document.getElementById("login-password").value;
+    
+    // Simulação de login (substitua por autenticação real)
+    if (username === "user" && password === "password") {
+        loggedInUser = username;
+        showProfile();
+    } else {
+        alert("Login inválido. Tente novamente.");
     }
 });
 
-messageInput.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-        sendButton.click();
-    }
+document.getElementById("register-button").addEventListener("click", function () {
+    const username = document.getElementById("register-username").value;
+    const password = document.getElementById("register-password").value;
+    
+    // Simulação de cadastro (substitua por armazenamento real)
+    alert(`Usuário "${username}" cadastrado com sucesso!`);
 });
+
+function showProfile() {
+    loginForm.style.display = "none";
+    registrationForm.style.display = "none";
+    profile.style.display = "block";
+
+    // Simulação de geração de foto de anime (substitua por lógica real)
+    const randomImageNumber = Math.floor(Math.random() * 10) + 1;
+    const imageUrl = `https://www.example.com/anime${randomImageNumber}.jpg`;
+    profileImage.src = imageUrl;
+}
