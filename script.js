437403d7bc618fa5bd5045de590ab68d21be2ce5
masterdevhomirinha <138265944+masterdevhomirinha@users.noreply.github.com>
2023-08-06 13:34:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,6 @@
-const animeImage = document.getElementById("anime-image");
-const generateButton = document.getElementById("generate-button");
-const userProfile = document.getElementById("user-profile");
-const usernameSpan = document.getElementById("username");
-const emailSpan = document.getElementById("email");
+const botao = document.getElementById('botao');
+const texto = document.getElementById('texto');
 
-let currentUser = null;
-
-// Simulação de dados de usuários (substitua por um backend real)
-const users = [
-    { username: "user1", email: "user1@example.com", password: "password1" },
-    { username: "user2", email: "user2@example.com", password: "password2" }
-];
-
-generateButton.addEventListener("click", generateImage);
-
-function generateImage() {
-    // ... Código de geração de imagem (veja o exemplo anterior) ...
-}
-
-function showProfile(user) {
-    currentUser = user;
-    usernameSpan.textContent = user.username;
-    emailSpan.textContent = user.email;
-    userProfile.style.display = "block";
-}
-
-document.getElementById("login-button").addEventListener("click", function () {
-    const username = document.getElementById("login-username").value;
-    const password = document.getElementById("login-password").value;
-
-    const user = users.find(u => u.username === username && u.password === password);
-    if (user) {
-        showProfile(user);
-    } else {
-        alert("Login inválido. Tente novamente.");
-    }
-});
-
-document.getElementById("register-button").addEventListener("click", function () {
-    const username = document.getElementById("register-username").value;
-    const email = document.getElementById("register-email").value;
-    const password = document.getElementById("register-password").value;
-
-    const newUser = { username, email, password };
-    users.push(newUser);
-
-    alert(`Usuário "${username}" cadastrado com sucesso!`);
-    showProfile(newUser);
+botao.addEventListener('click', () => {
+    texto.textContent = 'Texto alterado com sucesso!';
 });
